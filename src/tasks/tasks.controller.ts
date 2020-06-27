ba6406b309a62153d2356a90e35123032792355f
brunoN80 <bruno_rocha2008@hotmail.com>
2020-06-27 16:50:55 -0300

    search with query

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { getTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.taskService.getAlltasks()
+    getTasks(@Query() filterDto: getTasksFilterDTO): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.taskService.getTasksWithFilters(filterDto)
+        } else {
+            return this.taskService.getAlltasks()
+        }
     }
 
     @Get('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid'
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { getTasksFilterDTO } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,25 @@ export class TasksService {
 
     getAlltasks(): Task[] {
         return this.tasks
+    }
+
+    getTasksWithFilters(filterDto: getTasksFilterDTO): Task[] {
+        const { status, search } = filterDto
+
+        let tasks = this.getAlltasks()
+
+        if(status) {
+            tasks = tasks.filter(task => task.status === status)
+        }
+
+        if (search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)    
+            )
+        }
+
+        return tasks
     }
 
     getTaskById(id: string): Task {
